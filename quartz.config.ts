3a63542a8bbbc0ef08b7e11a2d414de77d3710f1
nishadalal <nisha.dalal@gmx.net>
2025-08-10 21:45:09 +0200

    fixing the homepage

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://nishadalal.com",
+    baseUrl: "nishadalal.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
